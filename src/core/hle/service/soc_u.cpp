4061042082c2dcb552441c9916e0deabcca305be
Subv <subv2112@gmail.com>
2014-12-09 10:05:49 -0500

    SOC_U: Implemented more functions
    This is enough to get FTPony to a working state

--- src/core/hle/service/soc_u.cpp
+++ src/core/hle/service/soc_u.cpp
@@ -2,12 +2,16 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
-#ifdef _WIN32
+#if EMU_PLATFORM == PLATFORM_WINDOWS
 #pragma comment(lib, "Ws2_32.lib")
 #include <winsock2.h>
 #include <ws2tcpip.h>
-else
+#else
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
 #endif
 
 #include "common/log.h"
@@ -50,8 +54,10 @@ static void SOCU_bind(Service::Interface* self) {
     SOCKET sock = (SOCKET)cmd_buffer[1];
     u32 len = cmd_buffer[2];
     sockaddr* sock_addr = reinterpret_cast<sockaddr*>(Memory::GetPointer(cmd_buffer[6]));
-    sock_addr->sa_family >>= 8; // Discard the low 8 bits (length)
-
+#if EMU_PLATFORM != PLATFORM_MACOSX
+    // OS X uses the first byte for the struct length, Windows doesn't
+    sock_addr->sa_family >>= 8; 
+#endif
     cmd_buffer[2] = bind(sock, sock_addr, sizeof(sockaddr_in));
     cmd_buffer[1] = 0;
 }
@@ -63,7 +69,7 @@ static void SOCU_fcntl(Service::Interface* self) {
     u32 arg = cmd_buffer[3];
 
     u32 ret;
-#ifdef _WIN32
+#if EMU_PLATFORM == PLATFORM_WINDOWS
     // Windows uses different enum values with different meanings,
     // cmd 4 is F_SETFL and arg 4 is O_NONBLOCK
     if (cmd == 4 && arg == 4) {
@@ -105,8 +111,52 @@ static void SOCU_accept(Service::Interface* self) {
 static void SOCU_gethostid(Service::Interface* self) {
     u32* cmd_buffer = Service::GetCommandBuffer();
 
-    // TODO(Subv): Implement
-    cmd_buffer[2] = 0;
+    char name[128];
+    gethostname(name, sizeof(name));
+    hostent* host = gethostbyname(name);
+    in_addr* addr = reinterpret_cast<in_addr*>(host->h_addr);
+
+    cmd_buffer[2] = addr->s_addr;
+    cmd_buffer[1] = 0;
+}
+
+static void SOCU_close(Service::Interface* self) {
+    u32* cmd_buffer = Service::GetCommandBuffer();
+    SOCKET sock = (SOCKET)cmd_buffer[1];
+
+#if EMU_PLATFORM == PLATFORM_WINDOWS
+    cmd_buffer[2] = closesocket(sock);
+#else
+    cmd_buffer[2] = close(sock);
+#endif
+    cmd_buffer[1] = 0;
+}
+
+static void SOCU_sendto(Service::Interface* self) {
+    u32* cmd_buffer = Service::GetCommandBuffer();
+    SOCKET sock = (SOCKET)cmd_buffer[1];
+    u32 len = cmd_buffer[2];
+    u32 flags = cmd_buffer[3];
+    u32 addr_len = cmd_buffer[4];
+
+    u8* input_buff = Memory::GetPointer(cmd_buffer[8]);
+    sockaddr* dest_addr = reinterpret_cast<sockaddr*>(Memory::GetPointer(cmd_buffer[10]));
+
+    cmd_buffer[2] = sendto(sock, (const char*)input_buff, len, flags, dest_addr, addr_len);
+    cmd_buffer[1] = 0;
+}
+
+static void SOCU_recvfrom(Service::Interface* self) {
+    u32* cmd_buffer = Service::GetCommandBuffer();
+    SOCKET sock = (SOCKET)cmd_buffer[1];
+    u32 len = cmd_buffer[2];
+    u32 flags = cmd_buffer[3];
+    int addr_len = cmd_buffer[4];
+
+    u8* output_buff = Memory::GetPointer(cmd_buffer[0x104 >> 2]);
+    sockaddr* src_addr = reinterpret_cast<sockaddr*>(Memory::GetPointer(cmd_buffer[0x1A0 >> 2]));
+
+    cmd_buffer[2] = recvfrom(sock, (char*)output_buff, len, flags, src_addr, &addr_len);
     cmd_buffer[1] = 0;
 }
 
@@ -118,10 +168,10 @@ const Interface::FunctionInfo FunctionTable[] = {
     {0x00050084, SOCU_bind,                     "bind"},
     {0x00060084, nullptr,                       "connect"},
     {0x00070104, nullptr,                       "recvfrom_other"},
-    {0x00080102, nullptr,                       "recvfrom"},
+    {0x00080102, SOCU_recvfrom,                 "recvfrom"},
     {0x00090106, nullptr,                       "sendto_other"},
-    {0x000A0106, nullptr,                       "sendto"},
-    {0x000B0042, nullptr,                       "close"},
+    {0x000A0106, SOCU_sendto,                   "sendto"},
+    {0x000B0042, SOCU_close,                    "close"},
     {0x000C0082, nullptr,                       "shutdown"},
     {0x000D0082, nullptr,                       "gethostbyname"},
     {0x000E00C2, nullptr,                       "gethostbyaddr"},
@@ -149,14 +199,14 @@ const Interface::FunctionInfo FunctionTable[] = {
 
 Interface::Interface() {
     Register(FunctionTable, ARRAY_SIZE(FunctionTable));
-#ifdef _WIN32
+#if EMU_PLATFORM == PLATFORM_WINDOWS
     WSADATA data;
     WSAStartup(MAKEWORD(2, 2), &data);
 #endif // _WIN32
 }
 
 Interface::~Interface() {
-#ifdef _WIN32
+#if EMU_PLATFORM == PLATFORM_WINDOWS
     WSACleanup();
 #endif
 }
