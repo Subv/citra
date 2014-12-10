51cf52abbdecd8ff464219b0a2178383492acc37
Subv <subv2112@gmail.com>
2014-12-10 00:45:40 -0500

    SOC_U: Implemented more functions.
    This allows ftbrony to accept connections, but some error in Poll is causing problems.
    
    Note: I'm starting to hate this code

--- src/core/hle/service/soc_u.cpp
+++ src/core/hle/service/soc_u.cpp
@@ -7,6 +7,7 @@
 #if EMU_PLATFORM == PLATFORM_WINDOWS
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <unordered_map>
 #else
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -32,6 +33,12 @@ struct hw_pollfd {
     u32 revents; ///< Events received (output)
 };
 
+#if EMU_PLATFORM == PLATFORM_WINDOWS
+/// Holds info about whether a specific socket is blocking or not
+/// This only exists on Windows because it has no way of querying if a socket is blocking or not
+std::unordered_map<u32, bool> socket_blocking;
+#endif
+
 static void Socket(Service::Interface* self) {
     u32* cmd_buffer = Service::GetCommandBuffer();
     u32 domain = cmd_buffer[1]; // Address family
@@ -53,7 +60,13 @@ static void Socket(Service::Interface* self) {
         return;
     }
 
-    cmd_buffer[2] = ::socket(domain, type, protocol);
+    u32 socket_handle = ::socket(domain, type, protocol);
+
+#if EMU_PLATFORM == PLATFORM_WINDOWS
+    socket_blocking[socket_handle] = true;
+#endif
+
+    cmd_buffer[2] = socket_handle;
     cmd_buffer[1] = 0;
 }
 
@@ -64,7 +77,8 @@ static void Bind(Service::Interface* self) {
     sockaddr* sock_addr = reinterpret_cast<sockaddr*>(Memory::GetPointer(cmd_buffer[6]));
 #if EMU_PLATFORM != PLATFORM_MACOSX
     // OS X uses the first byte for the struct length, Windows doesn't
-    sock_addr->sa_family >>= 8; 
+    if (sock_addr != nullptr)
+        sock_addr->sa_family >>= 8;
 #endif
     cmd_buffer[2] = ::bind(socket_handle, sock_addr, sizeof(sockaddr_in));
     cmd_buffer[1] = 0;
@@ -80,11 +94,16 @@ static void Fcntl(Service::Interface* self) {
 #if EMU_PLATFORM == PLATFORM_WINDOWS
     // Windows uses different enum values with different meanings,
     // cmd 4 is F_SETFL and arg 4 is O_NONBLOCK
-    if (cmd == 4 && arg == 4) {
+    if (cmd == 4 && arg & 4) {
         cmd = FIONBIO;
         arg = 1;
+        ret = ioctlsocket(socket_handle, cmd, reinterpret_cast<u_long*>(&arg));
+        socket_blocking[socket_handle] = false;
+    } else if (cmd == 3) {
+        // 3 is F_GETFL, this is used to check if a socket is nonblocking
+        // Return O_NONBLOCK (4) if the socket is nonblocking
+        ret = socket_blocking[socket_handle] ? 0 : 4;
     }
-    ret = ioctlsocket(socket_handle, cmd, reinterpret_cast<u_long*>(&arg));
 #else
     ret = ::fcntl(socket_handle, cmd, arg);
 #endif // _WIN32
@@ -115,8 +134,12 @@ static void Accept(Service::Interface* self) {
     addr.sa_family = (addr.sa_family << 8) | max_addr_len;
 #endif
 
-    // TODO(Subv): Write addr to the pointer located at cmd_buffer[0x104 >> 2]
+#if EMU_PLATFORM == PLATFORM_WINDOWS
+    if (ret != SOCKET_ERROR)
+        socket_blocking[ret] = true;
+#endif
 
+    Memory::WriteBlock(cmd_buffer[0x104 >> 2], reinterpret_cast<const u8*>(&addr), max_addr_len);
     cmd_buffer[2] = ret;
     cmd_buffer[1] = 0;
 }
@@ -139,6 +162,7 @@ static void Close(Service::Interface* self) {
 
 #if EMU_PLATFORM == PLATFORM_WINDOWS
     cmd_buffer[2] = ::closesocket(socket_handle);
+    socket_blocking.erase(socket_handle);
 #else
     cmd_buffer[2] = ::close(socket_handle);
 #endif
@@ -176,7 +200,8 @@ static void RecvFrom(Service::Interface* self) {
     cmd_buffer[2] = ::recvfrom(socket_handle, (char*)output_buff, len, flags, src_addr, &addr_len);
 #if EMU_PLATFORM != PLATFORM_MACOSX
     // OS X uses the first byte for the struct length, Windows doesn't
-    src_addr->sa_family = (src_addr->sa_family << 8) | addr_len;
+    if (src_addr != nullptr)
+        src_addr->sa_family = (src_addr->sa_family << 8) | addr_len;
 #endif
     cmd_buffer[1] = 0;
 }
@@ -259,6 +284,63 @@ static void Poll(Service::Interface* self) {
     cmd_buffer[2] = ret;
 }
 
+static void GetSockName(Service::Interface* self) {
+    u32* cmd_buffer = Service::GetCommandBuffer();
+    u32 socket_handle = cmd_buffer[1];
+    int len = cmd_buffer[2];
+
+    sockaddr* dest_addr = reinterpret_cast<sockaddr*>(Memory::GetPointer(cmd_buffer[0x104 >> 2]));
+
+    cmd_buffer[2] = ::getsockname(socket_handle, dest_addr, &len);
+#if EMU_PLATFORM != PLATFORM_MACOSX
+    // OS X uses the first byte for the struct length, Windows doesn't
+    if (dest_addr != nullptr)
+        dest_addr->sa_family = (dest_addr->sa_family << 8) | len;
+#endif
+    cmd_buffer[1] = 0;
+}
+
+static void Shutdown(Service::Interface* self) {
+    u32* cmd_buffer = Service::GetCommandBuffer();
+    u32 socket_handle = cmd_buffer[1];
+    int how = cmd_buffer[2];
+
+    cmd_buffer[2] = ::shutdown(socket_handle, how);
+    cmd_buffer[1] = 0;
+}
+
+static void GetPeerName(Service::Interface* self) {
+    u32* cmd_buffer = Service::GetCommandBuffer();
+    u32 socket_handle = cmd_buffer[1];
+    int len = cmd_buffer[2];
+
+    sockaddr* dest_addr = reinterpret_cast<sockaddr*>(Memory::GetPointer(cmd_buffer[0x104 >> 2]));
+
+    cmd_buffer[2] = ::getpeername(socket_handle, dest_addr, &len);
+#if EMU_PLATFORM != PLATFORM_MACOSX
+    // OS X uses the first byte for the struct length, Windows doesn't
+    if (dest_addr != nullptr)
+        dest_addr->sa_family = (dest_addr->sa_family << 8) | len;
+#endif
+    cmd_buffer[1] = 0;
+}
+
+static void Connect(Service::Interface* self) {
+    u32* cmd_buffer = Service::GetCommandBuffer();
+    u32 socket_handle = cmd_buffer[1];
+    socklen_t len = cmd_buffer[2];
+
+    sockaddr* input_addr = reinterpret_cast<sockaddr*>(Memory::GetPointer(cmd_buffer[6]));
+
+    cmd_buffer[2] = ::connect(socket_handle, input_addr, len);
+#if EMU_PLATFORM != PLATFORM_MACOSX
+    // OS X uses the first byte for the struct length, Windows doesn't
+    if (input_addr != nullptr)
+        input_addr->sa_family = (input_addr->sa_family << 8) | len;
+#endif
+    cmd_buffer[1] = 0;
+}
+
 static void InitializeSockets(Service::Interface* self) {
     // TODO(Subv): Implement
 #if EMU_PLATFORM == PLATFORM_WINDOWS
@@ -274,6 +356,7 @@ static void ShutdownSockets(Service::Interface* self) {
     // TODO(Subv): Implement
 #if EMU_PLATFORM == PLATFORM_WINDOWS
     WSACleanup();
+    socket_blocking.clear();
 #endif
 
     u32* cmd_buffer = Service::GetCommandBuffer();
@@ -286,13 +369,13 @@ const Interface::FunctionInfo FunctionTable[] = {
     {0x00030082, Listen,                        "Listen"},
     {0x00040082, Accept,                        "Accept"},
     {0x00050084, Bind,                          "Bind"},
-    {0x00060084, nullptr,                       "Connect"},
+    {0x00060084, Connect,                       "Connect"},
     {0x00070104, nullptr,                       "recvfrom_other"},
     {0x00080102, RecvFrom,                      "RecvFrom"},
     {0x00090106, nullptr,                       "sendto_other"},
     {0x000A0106, SendTo,                        "SendTo"},
     {0x000B0042, Close,                         "Close"},
-    {0x000C0082, nullptr,                       "Shutdown"},
+    {0x000C0082, Shutdown,                      "Shutdown"},
     {0x000D0082, nullptr,                       "GetHostByName"},
     {0x000E00C2, nullptr,                       "GetHostByAddr"},
     {0x000F0106, nullptr,                       "unknown_resolve_ip"},
@@ -302,8 +385,8 @@ const Interface::FunctionInfo FunctionTable[] = {
     {0x00140084, Poll,                          "Poll"},
     {0x00150042, nullptr,                       "SockAtMark"},
     {0x00160000, GetHostId,                     "GetHostId"},
-    {0x00170082, nullptr,                       "GetSockName"},
-    {0x00180082, nullptr,                       "GetPeerName"},
+    {0x00170082, GetSockName,                   "GetSockName"},
+    {0x00180082, GetPeerName,                   "GetPeerName"},
     {0x00190000, ShutdownSockets,               "ShutdownSockets"},
     {0x001A00C0, nullptr,                       "GetNetworkOpt"},
     {0x001B0040, nullptr,                       "ICMPSocket"},
@@ -322,6 +405,9 @@ Interface::Interface() {
 }
 
 Interface::~Interface() {
+#if EMU_PLATFORM == PLATFORM_WINDOWS
+    socket_blocking.clear();
+#endif
 }
 
 } // namespace
