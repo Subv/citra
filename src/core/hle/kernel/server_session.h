fa6c0646e6a6751b8198711cdd15e41b9bb23004
Subv <subv2112@gmail.com>
2016-05-22 19:20:47 -0500

    ServerSession: Update the comment for ServerSession.

--- src/core/hle/kernel/server_session.h
+++ src/core/hle/kernel/server_session.h
@@ -68,26 +68,16 @@ class ClientSession;
 class ClientPort;
 
 /**
- * Kernel object representing the client endpoint of an IPC session. Sessions are the basic CTR-OS
+ * Kernel object representing the server endpoint of an IPC session. Sessions are the basic CTR-OS
  * primitive for communication between different processes, and are used to implement service calls
  * to the various system services.
  *
  * To make a service call, the client must write the command header and parameters to the buffer
  * located at offset 0x80 of the TLS (Thread-Local Storage) area, then execute a SendSyncRequest
- * SVC call with its Session handle. The kernel will read the command header, using it to marshall
+ * SVC call with its ClientSession handle. The kernel will read the command header, using it to marshall
  * the parameters to the process at the server endpoint of the session. After the server replies to
  * the request, the response is marshalled back to the caller's TLS buffer and control is
  * transferred back to it.
- *
- * In Citra, only the client endpoint is currently implemented and only HLE calls, where the IPC
- * request is answered by C++ code in the emulator, are supported. When SendSyncRequest is called
- * with the session handle, this class's SyncRequest method is called, which should read the TLS
- * buffer and emulate the call accordingly. Since the code can directly read the emulated memory,
- * no parameter marshalling is done.
- *
- * In the long term, this should be turned into the full-fledged IPC mechanism implemented by
- * CTR-OS so that IPC calls can be optionally handled by the real implementations of processes, as
- * opposed to HLE simulations.
  */
 class ServerSession : public WaitObject {
 public:
